b08a0f42a1c8d9f0dbcc34a20a57e2fa44622225
Junyeong Park <junb0sday@gmail.com>
2020-11-13 19:05:26 +0900

    인게임 스테이트 필수 함수 추가
    업데이트, 드로우, 핸들이벤트 함수 추가로 실행 및 종료가 가능하게 함

--- TermProject/ingame_state.py
+++ TermProject/ingame_state.py
@@ -7,3 +7,23 @@ canvas_height = 720
 
 def enter():
     gfw.world.init(['bg', 'bullet' , 'character', 'ui'])
+
+def update():
+    gfw.world.update()
+
+def draw():
+    gfw.world.draw()
+    # gobj.draw_collision_box()
+
+def handle_event(e):
+    if e.type == SDL_QUIT:
+        gfw.quit()
+    elif e.type == SDL_KEYDOWN:
+        if e.key == SDLK_ESCAPE:
+            gfw.pop()
+
+def exit():
+    pass
+
+if __name__ == '__main__':
+    gfw.run_main()
