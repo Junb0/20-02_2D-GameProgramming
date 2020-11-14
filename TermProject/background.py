827d802426ce7b3254adb1841f9bcbc42d4ae09d
Junyeong Park <junb0sday@gmail.com>
2020-11-14 20:02:30 +0900

    background 클래스 수정
    현재 상황에서 불필요한 변수 및 기능 삭제, 업데이트시 pass 하도록 수정

--- TermProject/background.py
+++ TermProject/background.py
@@ -9,10 +9,8 @@ class Background:
         self.cw, self.ch = get_canvas_width(), get_canvas_height()
         self.win_rect = 0, 0, self.cw, self.ch
         self.center = self.image.w // 2, self.image.h // 2
-        hw, hh = self.cw // 2, self.    ch // 2
+
     def draw(self):
         self.image.clip_draw_to_origin(*self.win_rect, 0, 0)
     def update(self):
-        sl = round(self.cw / 2)
-        sb = round( self.ch / 2)
-        self.win_rect = sl, sb, self.cw, self.ch
+        pass
